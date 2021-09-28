dbd9edc338d1930fdfc2cc295fdc2344b023c4f2
shekharmitra <shekharmitra10@gmail>
2021-09-29 01:23:47 +0530

    test update

--- src/test/java/RestaurantTest.java
+++ src/test/java/RestaurantTest.java
@@ -69,6 +69,7 @@ class RestaurantTest {
         assertThrows(itemNotFoundException.class,
                 ()->restaurant.removeFromMenu("French fries"));
     }
+
     //<<<<<<<<<<<<<<<<<<<<<<<MENU>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 
